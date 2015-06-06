f381329b225f76b77842cbc049a6ef7206896ad8
Malcolm <mbalchcr@oberlin.edu>
2015-06-05 21:48:19 -0400

    fix urls, configure shell_plus, set up house view and template

--- livewithfriends/manage.py
+++ livewithfriends/manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+# Enable tab completion in shell_plus
+import readline
+import rlcompleter
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "livewithfriends.settings")
 
--- livewithfriends/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^expenses/', include('expenses.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
