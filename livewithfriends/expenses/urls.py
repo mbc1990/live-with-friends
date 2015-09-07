6210882d0c7ed2b54e142f8f21b7e8c65d9eb0be
Malcolm <mbalchcr@oberlin.edu>
2015-09-06 21:34:22 -0400

    add dashboard view and template, urls, migration

--- livewithfriends/expenses/urls.py
+++ livewithfriends/expenses/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from views import HouseView
+from views import HouseView, DashboardView
 
 urlpatterns = [
+    url(r'^dashboard$', DashboardView.as_view(), name='dashboard'),
     url(r'^house/(?P<pk>\d+)/$', HouseView.as_view(), name='house'),
 ]
--- livewithfriends/expenses/views.py
+++ livewithfriends/expenses/views.py
@@ -1,11 +1,24 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView 
+from django.views.generic import DetailView, TemplateView
+
 from expenses.models import House
 
 
 def index(request):
     return HttpResponse("Hello world")
+
+class DashboardView (TemplateView):
+    template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['date'] = datetime.date.today()
+        return context
+
+    
 
 class HouseView(DetailView):
     template_name = "house.html"
@@ -13,5 +26,5 @@ class HouseView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(HouseView, self).get_context_data(**kwargs)
-        context['test_var'] = 'hello context'
+        context['hello'] = 'hello context'
         return context
